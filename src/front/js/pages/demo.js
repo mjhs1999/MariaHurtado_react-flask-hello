be3d8fa2e0b5c0413dbc9f230901efb492de1f0a
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2024-01-10 19:12:32 +0000

    FLUX IS UPDATED

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -1,41 +1,31 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+const navigate = useNavigate();
 
-import { Context } from "../store/appContext";
+useEffect(() => {
+  if (store.token == undefined) {
+	navigate("/");
+  }
+}, []);
 
-export const Demo = () => {
-	const { store, actions } = useContext(Context);
+const handleLogOut = () => {
+  actions.LogOut();
+  navigate("/");
+};
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
+console.log(store.informationUser);
+
+return (
+  <React.Fragment>
+	<div className="container mt-5">
+	  <h1 className="text-center">
+		<strong>You are logged</strong>
+	  </h1>
+	  <br />
+	</div>
+	<div className="close d-flex justify-content-center">
+	  <button type="button" className="btn btn-danger" onClick={handleLogOut}>
+		Log Out
+	  </button>
+	</div>
+  </React.Fragment>
+);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,147 @@
-const getState = ({ getStore, getActions, setStore }) => {
+State = ({ getStore, getActions, setStore }) => {
+	const url = "https://opulent-parakeet-7r6p6qgrrwp2wwwx-3001.app.github.dev/";
+  
 	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+	  store: {
+		message: null,
+		demo: [
+		  {
+			title: "FIRST",
+			background: "white",
+			initial: "white",
+		  },
+		  {
+			title: "SECOND",
+			background: "white",
+			initial: "white",
+		  },
+		],
+  
+		token: localStorage.getItem("token") ?? null,
+		informationUser: {},
+	  },
+	  actions: {
+		// Use getActions to call a function within a fuction
+		exampleFunction: () => {
+		  getActions().changeColor(0, "green");
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+  
+		getMessage: async () => {
+		  try {
+			// fetching data from the backend
+			const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+			const data = await resp.json();
+			setStore({ message: data.message });
+			// don't forget to return something, that is how the async resolves
+			return data;
+		  } catch (error) {
+			console.log("Error loading message from backend", error);
+		  }
+		},
+		changeColor: (index, color) => {
+		  //get the store
+		  const store = getStore();
+  
+		  //we have to loop the entire demo array to look for the respective index
+		  //and change its color
+		  const demo = store.demo.map((elm, i) => {
+			if (i === index) elm.background = color;
+			return elm;
+		  });
+  
+		  //reset the global store
+		  setStore({ demo: demo });
+		},
+  
+		/* Se crea un usuario siempre y cuando no exista  */
+  
+		CreateUser: async (event, email, password) => {
+		  event.preventDefault();
+  
+		  try {
+			const response = await fetch(url + "api/singup", {
+			  method: "POST",
+			  body: JSON.stringify({
+				email: email,
+				password: password,
+			  }),
+			  headers: {
+				"content-type": "Application/json",
+			  },
+			});
+  
+			if (response.ok) {
+			  return await response.json();
+			} else {
+			  return console.log("Usuario ya existe");
 			}
-		}
+		  } catch (error) {
+			console.log(error);
+		  }
+		},
+  
+		/* Creamos un token a partir de los datos del usaurio */
+  
+		LoginUser: async (event, email, password) => {
+		  const store = getStore();
+		  event.preventDefault();
+  
+		  try {
+			const response = await fetch(url + "api/login", {
+			  method: "POST",
+			  body: JSON.stringify({
+				email: email,
+				password: password,
+			  }),
+			  headers: {
+				"content-type": "Application/json",
+			  },
+			});
+  
+			if (response.ok) {
+			  const body = await response.json();
+			  setStore({ token: body.token });
+			  localStorage.setItem("token", body.token);
+			  return true;
+			}
+		  } catch (error) {
+			console.log(error);
+		  }
+		},
+  
+		/* Damos acceso a la ruta privada  */
+  
+		PrivateRute: async () => {
+		  const store = getStore();
+  
+		  try {
+			const response = await fetch(url + "api/private", {
+			  method: "GET",
+			  headers: {
+				Authorization: ` Bearer ${store.token}`,
+			  },
+			});
+  
+			if (response.ok) {
+			  const body = await response.json();
+			  setStore({ informationUser: body });
+			  return true;
+			}
+		  } catch (error) {
+			console.log(error);
+		  }
+		},
+  
+		SetToken: (token) => {
+		  setStore({ token });
+		},
+  
+		LogOut: () => {
+		  localStorage.removeItem("token");
+		  setStore({ token: null });
+		},
+	  },
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
